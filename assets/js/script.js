88c155d33af9cc8b664bbc16fa3a4fe8f08cfaad
𝐄𝐚𝐬𝐭𝐞𝐫𝐧 𝐆𝐮𝐫𝐮🍀 <168541653+jackim00@users.noreply.github.com>
2024-09-28 23:34:23 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["web development", "graphic design", "web designing", "android development",],
+    strings: ["web development", "graphic design", "web designing", "content creation",],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
